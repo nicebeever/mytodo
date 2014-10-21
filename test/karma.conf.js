4edfe29e15f984f149a691d103760f604f66a792
nicebeever <robert@nicebeever.com>
2014-10-21 21:19:15 +0100

    Test
    Tetsing

--- test/karma.conf.js
+++ test/karma.conf.js
@@ -26,6 +26,9 @@ module.exports = function(config) {
       'bower_components/angular-route/angular-route.js',
       'bower_components/angular-sanitize/angular-sanitize.js',
       'bower_components/angular-touch/angular-touch.js',
+      'bower_components/jquery/dist/jquery.js',
+	  'bower_components/jquery-ui/jquery-ui.js',
+	  'bower_components/angular-ui-sortable/sortable.js',
       'app/scripts/**/*.js',
       'test/mock/**/*.js',
       'test/spec/**/*.js'
@@ -66,10 +69,10 @@ module.exports = function(config) {
     logLevel: config.LOG_INFO,
 
     // Uncomment the following lines if you are using grunt's server to run the tests
-    // proxies: {
-    //   '/': 'http://localhost:9000/'
-    // },
+    proxies: {
+    '/': 'http://localhost:9000/'
+    },
     // URL root prevent conflicts with the site root
-    // urlRoot: '_karma_'
+    urlRoot: '_karma_'
   });
 };
